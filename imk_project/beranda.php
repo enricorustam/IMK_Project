fa6f66bddcfe45dbc0b73d9976abca4c5d0ed7c3
enricorustam <enricorustam@gmail.com>
2019-06-11 08:04:13 +0700

    Add files via upload

---
+++ imk_project/beranda.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sidebar</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.css" rel="stylesheet"/>
+  <link href="css/font-awesome.css" rel="stylesheet"/>
+  <link href="css/custom.css" rel="stylesheet"/>
+  <link href="css/beranda.css" rel="stylesheet"/>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <ul class="sidebar">
+      <li class="sidebar-brand"><a href="">Sidebar by Andrew</a></li>
+      <li><a href="#">Dashboard</a></li>
+      <li><a href="#">Support</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Logout</a></li>
+      </ul>
+  </body>
+</html>
---
+++ imk_project/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="css/bootstrap.css" rel="stylesheet"/>
+  <link href="css/font-awesome.css" rel="stylesheet"/>
+  <link href="css/custom.css" rel="stylesheet"/>
+  <link href="css/login.css" rel="stylesheet"/>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/login.js"></script>
+</head>
+<body>
+		<form id="loginform" class="form-signin">
+	  		<img src="https://www.e-qualcompetence.co.uk/img/ddl-logo.png" class="img-responsive img-rounded" alt="Logo do Projeto PSW Responsiva">
+			<h3 class="form-signin-heading">Pemodelan Basis Data</h3>
+	    	<label for="inputEmail" class="sr-only">Username</label>
+	    	<input type="text" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
+			<label for="inputPassword" class="sr-only">Password</label>
+	    	<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+	    	<div class="checkbox">
+		   		<label><input type="checkbox" value="remember-me">Remember me</label>
+	      	</div>
+	    	<button id="LoginBotao" class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+	    	<button id="CadastroBotao" class="btn btn-lg btn-success btn-block" type="Register">Register</button>
+		</form>
+</body>
+</html>
